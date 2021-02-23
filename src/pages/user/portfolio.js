eb89b69dc15aafd1b8e24c18429ce63a50888add
GreenrSpace <incompleteness_hewlbern@pop-os.localdomain>
2021-02-24 00:41:38 +1100

    small changes

--- src/pages/user/portfolio.js
+++ src/pages/user/portfolio.js
@@ -1,24 +1,24 @@
-import ContactSection from "components/Layout/contact/ContactSection";
-import Layout from "components/Layout/Layout";
+// import ContactSection from "components/Layout/contact/ContactSection";
+// import Layout from "components/Layout/Layout";
 
 
-function ContactPage(props) {
-  return (
-    <Layout title = "Moodmaps Contact Us page"  description = "An automated mood journal">
-    <ContactSection
-      bg="white"
+// function ContactPage(props) {
+//   return (
+//     <Layout title = "Moodmaps Contact Us page"  description = "An automated mood journal">
+//     <ContactSection
+//       bg="white"
     
-      title="Contact Us"
-      subtitle=""
-      buttonText="Send message"
-      buttonColor="primary"
-      showNameField={true}
-      inputSize="md"
-    />
-    </Layout>
-  );
-}
+//       title="Contact Us"
+//       subtitle=""
+//       buttonText="Send message"
+//       buttonColor="primary"
+//       showNameField={true}
+//       inputSize="md"
+//     />
+//     </Layout>
+//   );
+// }
 
-export default ContactPage;
+// export default ContactPage;
 
 
